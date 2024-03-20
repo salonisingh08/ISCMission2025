3612cc51f06cfda472c827366abd12c5fb2ed2ef
salonisingh08 <salonisingh082007@gmail.com>
2024-03-20 07:23:26 +0530

    Mirror Triangle Solution

---
+++ src/Pattern/MirrorTriangle.java
@@ -0,0 +1,37 @@
+package Pattern;
+
+/*
+1 2 3 4 5
+ 2 3 4 5
+  3 4 5
+   4 5
+    5
+    5
+   4 5
+  3 4 5
+ 2 3 4 5
+1 2 3 4 5
+
+ */
+public class MirrorTriangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <=5 ; i++) {
+            for (int j = 1; j <i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <=5 ; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        for (int i = 5; i >=1 ; i--) {
+            for (int j = 1; j <i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <=5 ; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
