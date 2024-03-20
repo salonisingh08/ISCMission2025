2e58733f3edf61ea76fc2738cf3378c63a47173e
salonisingh08 <salonisingh082007@gmail.com>
2024-03-20 07:16:45 +0530

    Increase Column Solution

---
+++ src/Pattern/IncreaseColumn.java
@@ -0,0 +1,20 @@
+package Pattern;
+/*
+        1
+        2 1
+        3 2 1
+        4 3 2 1
+        5 4 3 2 1
+ */
+public class IncreaseColumn {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
