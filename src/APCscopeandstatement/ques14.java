d5df49d7b2219832fd51f73c9368edc8722c3b27
salonisingh08 <salonisingh082007@gmail.com>
2024-04-18 20:55:58 +0530

    Question 14 Solution

---
+++ src/APCscopeandstatement/ques14.java
@@ -0,0 +1,44 @@
+package APCscopeandstatement;
+
+public class Prog14 {
+    public static boolean isFascinating(
+            int num)
+    {
+        int[] freq = new int[10];
+        String val = "" + num + num * 2 +
+                num * 3;
+        for (int i = 0; i < val.length(); i++)
+        {
+            int digit = val.charAt(i) - '0';
+
+            if (freq[digit]>0 && digit != 0)
+                return false;
+            else
+                freq[digit]++;
+        }
+
+        for (int i = 1; i < freq.length; i++)
+        {
+            if (freq[i] == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String args[])
+    {
+        int num = 192;
+
+        if (num < 100)
+            System.out.println("No");
+
+        else
+        {
+            boolean ans = isFascinating(num);
+            if (ans)
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+        }
+    }
+}
